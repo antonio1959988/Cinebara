fe2d4d16c92700046199c34e2041e9c800b6d73f
antonio.trrsch <antonio1959988@hotmail.com>
2024-05-03 15:08:43 -0600

    temporally-closed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,12 +15,14 @@ export default function Header() {
                     <div className="text-lg px-12">
                         ¡La mejor calidad del momento! ¡Sin anuncios ni publicidad!
                     </div>
+                   {/*
                     <form className=" items-center flex flex-col md:flex-row md:justify-center md:items-center md:gap-2">
 
                         <input type="text" placeholder="¿Que título estas buscando?" className="w-72 h-12 border-[2px] border-yellow-950 bg-white placeholder:text-yellow-950 px-4 mb-4 md:mb-0"/>
 
                         <input type="button" className="px-6 bg-yellow-950 w-36 h-12 font-extrabold uppercase text-white" value="Buscar" />
                     </form>
+                   */}
 
                     <div className="text-black md:text-2xl font-semibold">
                         Más de {movies.length} películas y series. Proximamente muchas más....
