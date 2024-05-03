6b0637c46cf1c8d5820869f9cef181797846b171
antonio.trrsch <antonio1959988@hotmail.com>
2024-05-03 09:11:26 -0600

    repair

--- src/components/ScrollContent.tsx
+++ src/components/ScrollContent.tsx
@@ -1,6 +1,6 @@
 
 import {MovieType} from '../types'
-import Slider from 'react-slick'
+import { Slider } from 'react-slick'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
